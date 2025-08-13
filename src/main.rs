9295ff80e814255b2b1e2d7c62363234e4fb9513
tommyrobot666 <tommyrobot10@gmail.com>
2025-08-12 20:43:16 -0700

    program fixed (it runs)

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@ use macroquad::prelude::*;
 //put "cargo add macroquad" in termanal to install lib
 #[macroquad::main("VerletRopes")]
 async fn main() {
-    // let &mut (mut points, mut lines) = &mut create_rope([0.0,0.0],-10.0,10);
-    // let points:&mut Vec<&mut Point> = &mut Line::extract_points(lines.to_vec());
-    let mut rope_data = create_rope([0.0, 0.0],-10.0,10);
+    let mut rope_data = create_rope([50.0, 50.0],-10.0,10);
     let (points, lines) = (&mut rope_data.0, &mut rope_data.1);
 
     loop {
         // update
-        simple_force_to_points(points, [0.0,1.0], 1.0);
-        simple_sim_step(lines);
+        if is_key_down(KeyCode::Q) {
+            simple_force_to_points(points, [0.0, 1.0], 1.0);
+            for _ in 0..1 {
+                simple_sim_step(lines, points)
+            }
+        };
         // draw
         clear_background(BLACK);
 
@@ -19,7 +21,8 @@ async fn main() {
         }
 
         for line in lines.iter() {
-            draw_line(line.a.x, line.a.y, line.b.x, line.b.y, 3.0, WHITE);
+            let (a,b) = line.get_both_points(points);
+            draw_line(a.x, a.y, b.x, b.y, 3.0, WHITE);
         }
 
         next_frame().await
@@ -42,38 +45,30 @@ impl Point {
     }
 }
 
-struct PointAndForce<'a> {
-    pub point:&'a mut Point,
-    pub force:[f32;2]
-}
-
-struct Line<'a> {
-    pub a: &'a mut Point,
-    pub b: &'a mut Point,
+struct Line {
+    pub a: usize,
+    pub b: usize,
     pub length:f32
 }
 
-impl<'a> Line<'a> {
-    fn extract_points(lines:Vec<Line<'a>>) -> Vec<&'a mut Point> {
-        let mut out:Vec<&'a mut Point> = Vec::with_capacity(lines.len());
-        for line in lines {
-            if !out.iter().any(|i| (*i as *const Point) == (line.a as *const Point)) {
-                out.push(line.a);
-            }
-            if !out.iter().any(|i| (*i as *const Point) == (line.b as *const Point)) {
-                out.push(line.b);
-            }
+impl Line {
+    fn get_both_points<'a>(&self, points: &'a mut [Point]) -> (&'a mut Point, &'a mut Point) {
+        if self.a < self.b {
+            let (first, second) = points.split_at_mut(self.b);
+            (&mut first[self.a], &mut second[0])
+        } else {
+            let (first, second) = points.split_at_mut(self.a);
+            (&mut second[0], &mut first[self.b])
         }
-        out
     }
 }
 
-fn simple_forces_to_points(mut points:&mut Vec<PointAndForce>, delta:f32){
+fn simple_forces_to_points(points:&mut Vec<Point>, forces:Vec<[f32;2]>, delta:f32){
     for i in 0..points.len(){
-        let point = &mut points[i].point;
+        let point = &mut points[i];
         if point.locked{continue}
 
-        let force = &mut points[i].force;
+        let force = forces[i];
         let prev_pos:[f32;2] = [point.x,point.y];
         point.x += (point.x - point.px) + force[0] * delta * delta;
         point.y += (point.y - point.py) + force[1] * delta * delta;
@@ -96,41 +91,39 @@ fn simple_force_to_points(points:&mut Vec<Point>, force:[f32;2], delta:f32) {
 }
 
 
-fn simple_sim_step(lines:&mut Vec<Line>){
+fn simple_sim_step(lines:&mut Vec<Line>,points:&mut Vec<Point>){
     for i in 0..lines.len() {
         let line:&mut Line = &mut lines[i];
-        let center:[f32;2] = [(line.a.x+line.b.x)/2.0,(line.a.y+line.b.y)/2.0];
-        let mut dir:[f32;2] = [line.a.x-line.b.x,line.a.y-line.b.y];
+        let (a,b) = line.get_both_points(points);
+        let center:[f32;2] = [(a.x+b.x)/2.0,(a.y+b.y)/2.0];
+        let mut dir:[f32;2] = [a.x-b.x,a.y-b.y];
         // normalize dir
         let dir_len:f32 = (dir[0]*dir[0]+dir[1]*dir[1]).sqrt(); dir[0] = dir[0]/dir_len; dir[1] = dir[1]/dir_len;
 
-        if !line.a.locked{
-            line.a.x = center[0] + (dir[0] * line.length) / 2.0;
-            line.a.y = center[1] + (dir[1] * line.length) / 2.0;
+        if !a.locked{
+            a.x = center[0] + (dir[0] * line.length) / 2.0;
+            a.y = center[1] + (dir[1] * line.length) / 2.0;
         }
-        if !line.b.locked{
-            line.b.x = center[0] - (dir[0] * line.length) / 2.0;
-            line.b.y = center[1] - (dir[1] * line.length) / 2.0;
+        if !b.locked{
+            b.x = center[0] - (dir[0] * line.length) / 2.0;
+            b.y = center[1] - (dir[1] * line.length) / 2.0;
         }
     }
 }
 
 
-fn create_rope<'a>(start:[f32;2],length:f32,lines:i8)-> (Vec<Point>, Vec<Line<'a>>) {
+fn create_rope(start:[f32;2],length:f32,lines:usize)-> (Vec<Point>, Vec<Line>) {
     let mut next_pos:[f32;2] = [start[0]+length,start[1]];
-    let mut rope:Vec<Line<'a>> = Vec::with_capacity(lines as usize);
-    let mut points:Vec<Point> = Vec::with_capacity(lines as usize + 1);
+    let mut points:Vec<Point> = Vec::with_capacity(lines + 1);
     points.push(Point::new(start[0],start[1],true));
-    let mut last_point:&Point = points.get(0).unwrap();
-
-
     for _ in 0..lines {
         points.push(Point::new(next_pos[0], next_pos[1], false));
-        let mut current_point = points.get(points.len()-1).unwrap();
         next_pos[0] += length;
-        rope.push(Line{a: &mut current_point, b: &mut last_point, length });
+    }
 
-        last_point = current_point;
+    let mut rope:Vec<Line> = Vec::with_capacity(lines);
+    for i in 0..lines {
+        rope.push(Line{a: i, b: i + 1, length });
     }
 
     (points, rope)
