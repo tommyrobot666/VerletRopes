3f5abe0b6d1e39eaf26ff547d992f3c5681b04e1
tommyrobot666 <tommyrobot10@gmail.com>
2025-10-26 00:23:42 -0700

    Move all code to sepperate files

---
+++ src/dim2d.rs
@@ -0,0 +1,144 @@
+use crate::verletcore::{Line};
+
+pub struct Point {
+    pub x:f32,
+    pub y:f32,
+    pub px:f32,
+    pub py:f32,
+    pub locked:bool,
+}
+
+impl Point {
+    pub fn new(x:f32, y:f32, locked:bool) -> Point {
+        Point {
+            x,y,px:x,py:y,locked
+        }
+    }
+}
+
+
+pub struct AABB {
+    pub x:f32,
+    pub y:f32,
+    pub width:f32,
+    pub height:f32
+}
+
+impl AABB {
+    pub fn in_box(&self, point: &Point) -> bool {
+        point.x >= self.x && point.x <= self.x+self.width && point.y >= self.y && point.y <= self.y+self.height
+    }
+}
+
+impl Line {
+    pub fn get_both_points<'a>(&self, points: &'a mut [Point]) -> (&'a mut Point, &'a mut Point) {
+        if self.a < self.b {
+            let (first, second) = points.split_at_mut(self.b);
+            (&mut first[self.a], &mut second[0])
+        } else {
+            let (first, second) = points.split_at_mut(self.a);
+            (&mut second[0], &mut first[self.b])
+        }
+    }
+}
+
+
+
+
+#[allow(dead_code)]
+pub fn simple_forces_to_points(points:&mut Vec<Point>, forces:Vec<[f32;2]>, delta:f32){
+    for i in 0..points.len(){
+        let point = &mut points[i];
+        if point.locked{continue}
+
+        let force = forces[i];
+        let prev_pos:[f32;2] = [point.x,point.y];
+        point.x += (point.x - point.px) + force[0] * delta * delta;
+        point.y += (point.y - point.py) + force[1] * delta * delta;
+        point.px = prev_pos[0];
+        point.py = prev_pos[1];
+    }
+}
+
+pub fn simple_force_to_points(points:&mut Vec<Point>, force:[f32;2], delta:f32) {
+    for i in 0..points.len(){
+        let point = &mut points[i];
+        if point.locked{continue}
+
+        let prev_pos:[f32;2] = [point.x,point.y];
+        point.x += (point.x - point.px) + force[0] * delta * delta;
+        point.y += (point.y - point.py) + force[1] * delta * delta;
+        point.px = prev_pos[0];
+        point.py = prev_pos[1];
+    }
+}
+
+
+pub fn simple_sim_step(lines:&mut Vec<Line>,points:&mut Vec<Point>){
+    for i in 0..lines.len() {
+        let line:&mut Line = &mut lines[i];
+        let (a,b) = line.get_both_points(points);
+        let center:[f32;2] = [(a.x+b.x)/2.0,(a.y+b.y)/2.0];
+        let mut dir:[f32;2] = [a.x-b.x,a.y-b.y];
+        // normalize dir
+        let dir_len:f32 = (dir[0]*dir[0]+dir[1]*dir[1]).sqrt(); dir[0] = dir[0]/dir_len; dir[1] = dir[1]/dir_len;
+
+        if !a.locked{
+            a.x = center[0] + (dir[0] * line.length) / 2.0;
+            a.y = center[1] + (dir[1] * line.length) / 2.0;
+        }
+        if !b.locked{
+            b.x = center[0] - (dir[0] * line.length) / 2.0;
+            b.y = center[1] - (dir[1] * line.length) / 2.0;
+        }
+    }
+}
+
+
+pub fn aabb_collision(aabbs:&Vec<AABB>,points:&mut Vec<Point>){
+    for aabb in aabbs{
+        for point in &mut *points{
+            if aabb.in_box(&point){
+                let top_dist = (point.y-aabb.y).abs();
+                let left_dist = (point.x-aabb.x).abs();
+                let bottom_dist = (aabb.y+aabb.height-point.y).abs();
+                let right_dist = (aabb.x+aabb.width-point.x).abs();
+
+                if top_dist<left_dist && top_dist<bottom_dist && top_dist<right_dist {
+                    point.y = aabb.y
+                } else if left_dist<bottom_dist && left_dist<right_dist {
+                    point.x = aabb.x
+                } else if bottom_dist<right_dist {
+                    point.y = aabb.y+aabb.height
+                } else {
+                    point.x = aabb.x+aabb.width
+                }
+            }
+        }
+    }
+}
+
+
+pub fn create_rope(start:[f32;2], length:f32, lines:usize, pin_first:bool) -> (Vec<Point>, Vec<Line>) {
+    let mut next_pos:[f32;2] = [start[0]+length,start[1]];
+    let mut points:Vec<Point> = Vec::with_capacity(lines + 1);
+    points.push(Point::new(start[0], start[1], pin_first));
+    for _ in 0..lines {
+        points.push(Point::new(next_pos[0], next_pos[1], false));
+        next_pos[0] += length;
+    }
+
+    let mut rope:Vec<Line> = Vec::with_capacity(lines);
+    for i in 0..lines {
+        rope.push(Line{a: i, b: i + 1, length });
+    }
+
+    (points, rope)
+}
+
+pub fn offset_line_points(lines: &mut Vec<Line>, offset:usize){
+    for line in lines.iter_mut() {
+        line.a = line.a + offset;
+        line.b = line.b + offset;
+    }
+}
---
+++ src/verletcore.rs
@@ -0,0 +1,33 @@
+pub struct Line {
+    pub a: usize,
+    pub b: usize,
+    pub length:f32
+}
+
+pub enum ToolTypes {
+    Point,
+    Line,
+    LineOtherPoint,
+    RemovePoint,
+    MovePoint,
+    Lock,
+    Select,
+    AABB,
+    AABBOtherPoint
+}
+
+impl ToolTypes {
+    pub fn to_string(&self) -> &'static str {
+        match self {
+            ToolTypes::Select => {"Select"},
+            ToolTypes::MovePoint => {"Move Point"},
+            ToolTypes::Lock => {"Throw away the key"},
+            ToolTypes::Point => {"Add point (hold Tab to create chain)"},
+            ToolTypes::RemovePoint => {"Murder the point and hide the evidence"},
+            ToolTypes::Line => {"Start the creation of entire universes"},
+            ToolTypes::LineOtherPoint => {"You are now using a different tool!?!? (line ender)"}
+            ToolTypes::AABB => {"Make a purple box thing (or else)"},
+            ToolTypes::AABBOtherPoint => {"How big will this box be?"},
+        }
+    }
+}
